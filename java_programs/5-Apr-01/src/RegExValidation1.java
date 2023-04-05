bc426043778974037aa22d6982f02868deb5cba1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-04-05 10:18:19 +0530

    5-Apr 10:18

---
+++ java_programs/5-Apr-01/src/RegExValidation1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegExValidation1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String input=sc.next();
+//		System.out.println(input.matches("E\\d{3}"));
+//		Pattern p=Pattern.compile("E\\d{3}");
+		System.out.println(Pattern.matches("E\\d{3}", input));
+		
+	}
+
+}
