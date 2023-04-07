abeba3d26dcb98ebac032805aee954b9ea421565
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-04-07 09:54:26 +0530

    7-Apr 09:54

---
+++ java_programs/7-Apr-packages-1/src/one/Person.java
@@ -0,0 +1,22 @@
+package one;
+
+public class Person {
+	public void speak()
+	{
+		System.out.println("Person speaks");
+	}
+	void walk()
+	{
+		System.out.println("Person walks");
+	}
+	
+	protected void listen()
+	{
+		System.out.println("Person listens");
+	}
+	
+	private void run()
+	{
+		System.out.println("Person run");
+	}
+}
