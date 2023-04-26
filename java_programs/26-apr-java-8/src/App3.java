f666aec4f6fb51848fed274da72dbed43e2d8fa1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-04-26 11:39:29 +0530

    26-Apr 11:39

---
+++ java_programs/26-apr-java-8/src/App3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App3 {
+
+	public static void main(String[] args) {
+		List<Employee> empList=new ArrayList<Employee>();
+		empList.add(new Employee(248774,"Neethu"," Nelliparambil Rathimohan",12345.0));
+		empList.add(new Employee(249547,"Bhagyalakshmi"," Suresh",23451.0));
+		empList.add(new Employee(248825,"Ajay"," C M",45123.0));
+		empList.add(new Employee(249532,"kalyan"," Jalaneela",34512.0));
+		empList.add(new Employee(249529,"AISWARYA"," P",19000.0));
+		empList.add(new Employee(248764,"Ajayakrishnan"," J",10000.0));
+
+		
+	}
+
+}
---
+++ java_programs/26-apr-java-8/src/Employee.java
@@ -0,0 +1,56 @@
+
+public class Employee {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Double salary;
+	
+	public Employee() {}
+
+	public Employee(Integer id, String firstName, String lastName, Double salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ "]";
+	}
+	
+}
