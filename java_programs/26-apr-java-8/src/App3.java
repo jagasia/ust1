8c7bdde1c7775c4aef459ef0389de8cf3e4db8e2
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-04-26 11:44:31 +0530

    26-Apr 11:44

--- java_programs/26-apr-java-8/src/App3.java
+++ java_programs/26-apr-java-8/src/App3.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App3 {
 
@@ -12,7 +13,20 @@ public class App3 {
 		empList.add(new Employee(249529,"AISWARYA"," P",19000.0));
 		empList.add(new Employee(248764,"Ajayakrishnan"," J",10000.0));
 
+		//filter empList using salary>20000
+		//sort empList by salary
+		List<Employee> result = empList.stream()
+		.filter((a)->a.getSalary()>20000)
+//		.sorted((a,b)->a.getSalary().compareTo(b.getSalary()))
+		.sorted((a,b)->(int)(a.getSalary()-b.getSalary()))
+//		.forEach(System.out::println);
+		.collect(Collectors.toList());
 		
+//		System.out.println(result);
+		for(Employee e:result)
+		{
+			System.out.println(e);
+		}
 	}
 
 }
