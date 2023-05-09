dc18bbf23f25207ec1fdbd078c160df6d4f22879
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-05-09 16:54:15 +0530

    09-May 16:53

--- java_programs/08-may-strings/src/App2.java
+++ java_programs/08-may-strings/src/App2.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class App2 {
 
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat sdf=new SimpleDateFormat("dd-MMM-yyyy");
-		String str="08-May-2023";
-		Date dt=sdf.parse(str);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+		String str = "08-May-2023";
+		Date dt = sdf.parse(str);
 		System.out.println(dt);
 	}
 
