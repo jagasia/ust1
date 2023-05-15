65aa53203a104d3d614fe6ec943866aaeea63f6a
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-05-15 09:53:57 +0530

    15-May 09:53

---
+++ java_programs/11-may-split-space/src/Solution1.java
@@ -0,0 +1,9 @@
+public class Solution1{
+         public static void main(String args[]){
+                 int i;
+                 for(i = 1; i < 6; i++){ 
+                     if(i > 3) continue;
+                 }
+                 System.out.println(i);
+          }
+}
---
+++ java_programs/11-may-split-space/src/Solution2.java
@@ -0,0 +1,16 @@
+public class Solution2{
+       public static void main(String[] args){
+//                     byte x = 127;
+//                     x++;
+//                     x++;
+//                     System.out.print(x);
+
+int count = 0;
+do{
+  System.out.println("Interviewbit");
+  count++;
+} while(count < 10);
+
+
+       }
+}
