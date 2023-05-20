3a2fba891fa8c2d149f4ebb61c03be02a5349306
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-05-20 09:56:29 +0530

    20-May 09:56

---
+++ js/kalyan.js
@@ -0,0 +1,4 @@
+function fn2()
+{
+    alert('hello world... from hyderabad');
+}
---
+++ js/two.js
@@ -0,0 +1,13 @@
+var i=20;
+
+var j=30;
+var k=i+j;
+var i="raja";		//note, i is already declared. but no error
+console.log(i);
+k="fifty";
+let m="india";
+let n=200;
+console.log(m);
+//let m="bharath";		//note, m is already declared, now error
+m=20;
+console.log(m);
