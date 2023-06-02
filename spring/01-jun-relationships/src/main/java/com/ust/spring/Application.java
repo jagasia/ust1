b06ab7100c93dae040ab25dcb8588965f6bd8ac2
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-06-02 17:22:56 +0530

    02-Jun 17:22

--- spring/01-jun-relationships/src/main/java/com/ust/spring/Application.java
+++ spring/01-jun-relationships/src/main/java/com/ust/spring/Application.java
@@ -3,7 +3,10 @@ package com.ust.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	
 
