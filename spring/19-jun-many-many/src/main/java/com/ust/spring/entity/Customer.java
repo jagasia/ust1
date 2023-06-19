67d624bc1f2de2d1e01f1b7ef65eda1793b87128
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-06-19 10:43:37 +0530

    19-Jun 10:43

---
+++ spring/19-jun-many-many/src/main/java/com/ust/spring/entity/Customer.java
@@ -0,0 +1,76 @@
+package com.ust.spring.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "CUST")
+public class Customer {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@NotNull
+	private String name;
+	private String address;
+	@ManyToMany
+	private List<Product> products;
+	
+	public Customer() {}
+
+	public Customer(Integer id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+}
