d41998260fc407a4a1493abbf375e7cfbf2820e8
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-07-19 15:30:36 +0530

    19-Jul 15:30

---
+++ advanced/gateway-demo/src/test/java/com/ust/gateway/gatewaydemo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.gateway.gatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
