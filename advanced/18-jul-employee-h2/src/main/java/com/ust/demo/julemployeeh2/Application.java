f3d5c6b77f6c8f1276da60f8d7ea57d0cd89ea96
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-07-25 10:29:29 +0530

    25-Jul 10:29

--- advanced/18-jul-employee-h2/src/main/java/com/ust/demo/julemployeeh2/Application.java
+++ advanced/18-jul-employee-h2/src/main/java/com/ust/demo/julemployeeh2/Application.java
@@ -2,13 +2,11 @@ package com.ust.demo.julemployeeh2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
