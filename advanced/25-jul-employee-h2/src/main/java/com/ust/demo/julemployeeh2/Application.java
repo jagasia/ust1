70eaa490b81bbb257e13c0c1a9020c9954b69ee8
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-07-25 12:41:56 +0530

    25-Jul 12:41

---
+++ advanced/25-jul-employee-h2/src/main/java/com/ust/demo/julemployeeh2/Application.java
@@ -0,0 +1,17 @@
+package com.ust.demo.julemployeeh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Hello world");
+	}
+
+}
